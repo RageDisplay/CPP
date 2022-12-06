47823ee6844c9ef01b7c0fb2075b670b2f00b4f5
Dmitriy <86187895+RageDisplay@users.noreply.github.com>
2022-12-06 19:50:10 +0300

    Dijkstra

--- Dijkstra algorithm.cpp
+++ Dijkstra algorithm.cpp
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-//#define SIZE 6
 int main()
 {
   int SIZE;
@@ -10,53 +9,50 @@ int main()
   printf("До какой вершины нужен путь из 1 ?");
   int WAY;
   scanf("%d", &WAY);
-  int a[SIZE][SIZE]; // матрица связей
-  int d[SIZE]; // минимальное расстояние
-  int v[SIZE]; // посещенные вершины
+  int a[SIZE][SIZE]; 
+  int d[SIZE]; 
+  int v[SIZE];
   int temp, minindex, min;
   int begin_index = 0;
   system("chcp 1251");
   system("cls");
-  // Инициализация матрицы связей
   for (int i = 0; i<SIZE; i++)
   {
     a[i][i] = 0;
-    for (int j = i + 1; j<SIZE; j++) {
+    for (int j = i + 1; j<SIZE; j++) 
+    {
       printf("Введите расстояние %d - %d: ", i + 1, j + 1);
       scanf("%d", &temp);
       a[i][j] = temp;
       a[j][i] = temp;
     }
   }
-  // Вывод матрицы связей
   for (int i = 0; i<SIZE; i++)
   {
     for (int j = 0; j<SIZE; j++)
       printf("%5d ", a[i][j]);
     printf("\n");
   }
-  //Инициализация вершин и расстояний
   for (int i = 0; i<SIZE; i++)
   {
     d[i] = 10000;
     v[i] = 1;
   }
   d[begin_index] = 0;
-  // Шаг алгоритма
-  do {
+  
+  do 
+  {
     minindex = 10000;
     min = 10000;
     for (int i = 0; i<SIZE; i++)
-    { // Если вершину ещё не обошли и вес меньше min
+    { 
       if ((v[i] == 1) && (d[i]<min))
-      { // Переприсваиваем значения
+      { 
         min = d[i];
         minindex = i;
       }
     }
-    // Добавляем найденный минимальный вес
-    // к текущему весу вершины
-    // и сравниваем с текущим минимальным весом вершины
+    
     if (minindex != 10000)
     {
       for (int i = 0; i<SIZE; i++)
@@ -73,34 +69,34 @@ int main()
       v[minindex] = 0;
     }
   } while (minindex < 10000);
-  // Вывод кратчайших расстояний до вершин
+
   printf("\nКратчайшие расстояния до вершин: \n");
   for (int i = 0; i<SIZE; i++)
     printf("%5d ", d[i]);
 
-  // Восстановление пути
-  int ver[SIZE]; // массив посещенных вершин
-  int end = WAY-1; // индекс конечной вершины = 5 - 1
-  ver[0] = end + 1; // начальный элемент - конечная вершина
-  int k = 1; // индекс предыдущей вершины
-  int weight = d[end]; // вес конечной вершины
 
-  while (end != begin_index) // пока не дошли до начальной вершины
+  int ver[SIZE]; 
+  int end = WAY-1;
+  ver[0] = end + 1; 
+  int k = 1;
+  int weight = d[end]; 
+
+  while (end != begin_index) 
   {
-    for (int i = 0; i<SIZE; i++) // просматриваем все вершины
-      if (a[i][end] != 0)   // если связь есть
+    for (int i = 0; i<SIZE; i++)
+      if (a[i][end] != 0)
       {
-        int temp = weight - a[i][end]; // определяем вес пути из предыдущей вершины
-        if (temp == d[i]) // если вес совпал с рассчитанным
-        {                 // значит из этой вершины и был переход
-          weight = temp; // сохраняем новый вес
-          end = i;       // сохраняем предыдущую вершину
-          ver[k] = i + 1; // и записываем ее в массив
+        int temp = weight - a[i][end]; 
+        if (temp == d[i]) 
+        {                 
+          weight = temp; 
+          end = i;       
+          ver[k] = i + 1; 
           k++;
         }
       }
   }
-  // Вывод пути (начальная вершина оказалась в конце массива из k элементов)
+
   printf("\nВывод кратчайшего пути\n");
   for (int i = k - 1; i >= 0; i--)
     printf("%3d ", ver[i]);
